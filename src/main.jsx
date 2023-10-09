89ae2d3523c5b86e3f098138aba9c1364671c7d3
Rankoo <marlon971224@gmail.com>
2023-10-09 13:25:50 -0500

    adios app

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,20 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/*',
+    element: <NotFound />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />,
 )
