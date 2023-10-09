27d147c5c9aebb14fc7f016bae3ee6381a72c104
Rankoo <marlon971224@gmail.com>
2023-10-09 10:32:33 -0500

    otro arreglo

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  base: '/mathbirthdays/'
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [preact()],
+    base: '/mathbirthdays/'
+  }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
