c2f32c2ca3caeb871415522be5861c7ed5abee14
Rankoo <marlon971224@gmail.com>
2023-10-09 13:21:07 -0500

    fffff

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,22 @@
-import { Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 
+const routes = [
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/*',
+    element: <NotFound />
+  }
+]
+const router = createBrowserRouter(routes, { basename: '/mathbirthdays/' })
+
 const App = () => {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/*' element={<NotFound />} />
-      </Routes>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
